6367b8153f3bb026e4d01fcaeef368a486676d79
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-27 00:37:24 +0530

    Adding Pages to App.js

--- Public/src/App.js
+++ Public/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import Chat from './Pages/Chat'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
 
 export default function App() {
   return (
     <div>
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path='/register' element={<Register />}></Route>
+          <Route path='/login' element={<Login />}></Route>
+          <Route path='/' element={<Chat />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
