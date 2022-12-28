5b66addbce5656c5f8883a4d8815db387e811af6
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2022-12-28 23:59:33 +0530

    Using ExpressJS for Authentication API Transfer

--- Server/Index.js
+++ Server/Index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require("cors")
 const mongoose = require("mongoose")
+const userRoutes = require("./Routes/UserRoutes")
 
 const app = express()
 require("dotenv").config
 
 app.use(cors());
 app.use(express.json())
+
+app.use("/api/auth" , userRoutes)
 
 mongoose.connect(process.env.MONGO_URL , {
     useNewUrlParser: true, useUnifiedTopology: true
