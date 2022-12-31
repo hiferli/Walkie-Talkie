a4dd758466f03dac68d1ae0cdf9fa472f9028aa5
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-01 02:15:18 +0530

    Page Loading and Unselected Avatar Toast Added

--- Public/src/Pages/SetAvatar.jsx
+++ Public/src/Pages/SetAvatar.jsx
@@ -30,7 +30,14 @@ export default function SetAvatar() {
     theme: "dark",
   };
 
-  const setProfilePicture = async () => {};
+  const setProfilePicture = async () => {
+	console.log(setAvatars)
+    if (selectedAvatar === undefined) {
+      toast.error("Please Select One Avatar" , toastStyling)
+    } else {
+		
+	}
+  };
 
   useEffect(() => {
     (async () => {
@@ -42,7 +49,7 @@ export default function SetAvatar() {
           `${API}/${Math.round(Math.random() * 1000)}`
         );
 
-        const buffer = new Buffer (image.data);
+        const buffer = new Buffer(image.data);
         data.push(buffer.toString("base64"));
       }
 
@@ -57,83 +64,93 @@ export default function SetAvatar() {
 
   return (
     <>
+	{
+		isLoading ?
+		<Container> 
+			<img src={Loader} alt="loader" className="loader" />
+		</Container> : 
+
       <Container>
         <div className="title-container">
           <h1>Pick Up An Avatar</h1>
         </div>
         <div className="avatars">
-            {avatars.map((avatar, index) => {
-              return (
-                <div
-                  className={`avatar ${
-                    selectedAvatar === index ? "selected" : ""
-                  }`}
-                >
-                  <img
-                    src={`data:image/svg+xml;base64,${avatar}`}
-                    alt="avatar"
-                    key={avatar}
-                    onClick={() => setSelectedAvatar(index)}
-                  />
-                </div>
-              );
-            })}
-          </div>
+          {avatars.map((avatar, index) => {
+			  return (
+				  <div
+				  className={`avatar ${
+					  selectedAvatar === index ? "selected" : ""
+					}`}
+					>
+                <img
+                  src={`data:image/svg+xml;base64,${avatar}`}
+                  alt="avatar"
+                  key={avatar}
+                  onClick={() => setSelectedAvatar(index)}
+				  />
+              </div>
+            );
+          })}
+        </div>
+        <div className="submit-btn" onClick={setProfilePicture}>
+          Set As Profile Picture
+        </div>
       </Container>
+}
       <ToastContainer />
     </>
   );
 }
 
 const Container = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-gap: 3rem;
-background-color: #131324;
-height: 100vh;
-width: 100vw;
-.loader {
-  max-inline-size: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 3rem;
+  background-color: #131324;
+  height: 100vh;
+  width: 100vw;
+  .loader {
+    max-inline-size: 100%;
 }
 .title-container {
-  h1 {
-    color: white;
-  }
-}
-.avatars {
-  display: flex;
-  gap: 2rem;
-  .avatar {
-    border: 0.4rem solid transparent;
-    padding: 0.4rem;
-    border-radius: 5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    transition: 0.5s ease-in-out;
-    img {
-      height: 6rem;
-      transition: 0.5s ease-in-out;
+	h1 {
+      color: white;
     }
   }
-  .selected {
-    border: 0.4rem solid #4e0eff;
+  .avatars {
+    display: flex;
+    gap: 2rem;
+    .avatar {
+      border: 0.4rem solid transparent;
+      padding: 0.4rem;
+      border-radius: 5rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      transition: 0.5s ease-in-out;
+      img {
+        height: 6rem;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    .selected {
+      border: 0.4rem solid #4e0eff;
+    }
   }
-}
-.submit-btn {
-  background-color: #4e0eff;
-  color: white;
-  padding: 1rem 2rem;
-  border: none;
-  font-weight: bold;
-  cursor: pointer;
-  border-radius: 0.4rem;
-  font-size: 1rem;
-  text-transform: uppercase;
-  &:hover {
+  .submit-btn {
     background-color: #4e0eff;
+    color: white;
+    padding: 1rem 2rem;
+    border: none;
+    font-weight: bold;
+    cursor: pointer;
+    border-radius: 0.4rem;
+    font-size: 1rem;
+    text-transform: uppercase;
+    &:hover {
+      background-color: #4e0eff;
+    }
   }
-}
 `;
