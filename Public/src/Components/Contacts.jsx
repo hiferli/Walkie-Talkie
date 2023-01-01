52afa9ff6efbaa145026bb972812806d3c277110
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-01 22:49:45 +0530

    Display All Contacts from Backend via APIs #2

--- Public/src/Components/Contacts.jsx
+++ Public/src/Components/Contacts.jsx
@@ -1,24 +1,70 @@
-import React , { useState , useEffect } from 'react'
-import styled from 'styled-components'
-import Logo from '../Assets/Logo.svg'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Logo from "../Assets/Logo.svg";
 
-export default function Contacts({contacts , currentUser}) {
-    const [currentUserName, setCurrentUserName] = useState(undefined);
-    const [currentUserImage, setCurrentUserImage] = useState(undefined)
+export default function Contacts({ contacts, currentUser }) {
+  const [currentUserName, setCurrentUserName] = useState(undefined);
+  const [currentUserImage, setCurrentUserImage] = useState(undefined);
 
-    // For the selected Chat Option
-    const [currentSelected, setCurrentSelected] = useState(undefined)
+  // For the selected Chat Option
+  const [currentSelected, setCurrentSelected] = useState(undefined);
 
-    useEffect(() => {
-      if(currentUser){
-        setCurrentUserImage(currentUser.avatarImage);
-        setCurrentUserName(currentUser.username);
+  useEffect(() => {
+    if (currentUser) {
+      setCurrentUserImage(currentUser.avatarImage);
+      setCurrentUserName(currentUser.username);
     }
-    }, [currentUser])
-    
+  }, [currentUser]);
+
+  const changeCurrentChat = (index, contacts) => {};
+
   return (
-    <div>
-      Contacts
-    </div>
-  )
+    <>
+      {currentUserImage && currentUserImage && (
+        <Container>
+          <div className="brand">
+            <img src={Logo} alt="Logo" />
+            <h3>Walkie Talkie</h3>
+          </div>
+
+          <div className="contacts">
+            {contacts.map((contact, index) => {
+              return (
+                <div
+                  className={`contact ${
+                    index === currentSelected ? "selected" : ""
+                  }`}
+                  key={index}
+                >
+                  <div className="avatar">
+                    <img
+                      src={`data:image/svg+xml;base64,${contact.avatarImage}`}
+                      alt="avatar"
+                    />
+                  </div>
+                  <div className="username">
+                    <h3>{contact.username}</h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="current-user">
+            <div className="avatar">
+              <img
+                src={`data:image/svg+xml;base64,${currentUserImage}`}
+                alt="avatar"
+              />
+            </div>
+            <div className="username">
+              <h3>{currentUserName}</h3>
+            </div>
+          </div>
+        </Container>
+      )}
+    </>
+  );
 }
+
+const Container = styled.div``;
