ff137a72f05eb5f47691bc4d16c083ebec9dae64
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-02 01:12:07 +0530

    Creating Logout Component

--- Public/src/Components/Logout.jsx
+++ Public/src/Components/Logout.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import styled from "styled-components";
+import {BiPowerOff} from 'react-icons/bi'
 
 export default function Logout() {
+    const navigator = useNavigate();
+
+    const handleClick = async () => {
+        localStorage.clear();
+        navigator("/login")
+    }
+
   return (
-    <div>
-      
-    </div>
+    <Button>
+        <BiPowerOff />
+    </Button>
   )
 }
+
+const Button = styled.button``;
