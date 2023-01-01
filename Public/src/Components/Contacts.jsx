586ffa281cd737f44b707e7c7d4600c7c9ccff70
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-01 18:14:12 +0530

    Current User Contact Check in Contacts Component

--- Public/src/Components/Contacts.jsx
+++ Public/src/Components/Contacts.jsx
@@ -3,6 +3,19 @@ import styled from 'styled-components'
 import Logo from '../Assets/Logo.svg'
 
 export default function Contacts({contacts , currentUser}) {
+    const [currentUserName, setCurrentUserName] = useState(undefined);
+    const [currentUserImage, setCurrentUserImage] = useState(undefined)
+
+    // For the selected Chat Option
+    const [currentSelected, setCurrentSelected] = useState(undefined)
+
+    useEffect(() => {
+      if(currentUser){
+        setCurrentUserImage(currentUser.avatarImage);
+        setCurrentUserName(currentUser.username);
+    }
+    }, [currentUser])
+    
   return (
     <div>
       Contacts
