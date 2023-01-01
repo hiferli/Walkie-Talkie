db4d8e2c8e361430915d428fc06384767f3dda78
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-02 00:40:40 +0530

    Creating Frontend for Welcome Component + CSS

--- Public/src/Components/Welcome.jsx
+++ Public/src/Components/Welcome.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
-import Robot from '../Assets/Robot.gif'
+import React from "react";
+import styled from "styled-components";
+import Robot from "../Assets/Robot.gif";
 
 export default function Welcome({ currentUser }) {
   return (
     <Container>
-      
+      <img src={Robot} alt="Welcome Robot" />
+      {/* <h1>Welcome! <span>{currentUser}</span> </h1> */}
+      <h3>Start A Conversation By Selecting Fron The Panel Besides!</h3>
     </Container>
-  )
+  );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  flex-direction: column;
+  img {
+    height: 20rem;
+  }
+  span {
+    color: #4e0eff;
+  }
+`;
