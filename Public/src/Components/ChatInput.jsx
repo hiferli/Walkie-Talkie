d6390a8f047d99fc2c4cdf060fba6b89e84f47ef
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-02 14:03:22 +0530

    Constructing Type Message Box

--- Public/src/Components/ChatInput.jsx
+++ Public/src/Components/ChatInput.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React , { useState } from 'react'
 import styled from "styled-components";
+import Picker from 'emoji-picker-react'
+
+import { IoMdSend } from "react-icons/io";
+import { BsEmojiSmileFill  } from "react-icons/bs";
 
 export default function ChatInput(handleSendMessage) {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <div className="button-container">
+        <div className="emoji">
+          <BsEmojiSmileFill />
+        </div>
+      </div>
+
+      <form className='input-container'>
+        <input type="text" placeholder='Type Your Message Here...' />
+        <button className="submit">
+          <IoMdSend />
+        </button>
+      </form>
+    </Container>
   )
 }
+
+
+const Container = styled.div``;
