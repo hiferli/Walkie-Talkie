fc2562d5a805823e425c75f87f01803c18289556
IshaanJoshi <joshi.ishaan.2001@gmail.com>
2023-01-03 17:35:09 +0530

    Creating Message Adding to Database Functionality

--- Server/Controllers/MessagesController.js
+++ Server/Controllers/MessagesController.js
@@ -1,2 +1,27 @@
-module.exports.addMessage = async (request, response, next) => {}
+const messageModel = require("../Models/MessageModel")
+
+module.exports.addMessage = async (request, response, next) => {
+    try {
+        // Getting Message Information
+        const { from , to , body } = request.body;
+
+        // Creating the Mongo Collection to push in the Database
+        const data = await messageModel.create({
+            message: {
+                text: message
+            },
+            users: [from , to],
+            sender: from,
+        })
+
+        if(data) {
+            return response.json({message: "Message Added Successfully"})
+        } else {
+            return response.json({message: "Failed To Add The Message To The Database"})
+        }
+    } catch (exeption) {
+        next(exeption);
+    }
+}
+
 module.exports.getAllMessages = async (request, response, next) => {}
